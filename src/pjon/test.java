7a098799208bad7a9325aa6241893c206cacb4d6
Ubolwan Vorakanonta <nuitabing@gmail.com>
2022-07-28 21:11:02 -0700

    First commit

---
+++ src/pjon/test.java
@@ -0,0 +1,13 @@
+package pjon;
+
+public class test {
+	static void main(String[] args) {
+		callMyName("Nick", 18, "La-ongmani");
+	}
+	
+	static void callMyName(String name, int age, String lastName) {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(lastName);
+	}
+}
